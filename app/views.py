485ebcbe2e99e095245898eda88e51439bc87d9d
Mikael Svensson <mikael.svensson@klarna.com>
2017-01-06 10:16:35 +0100

    First attempt at Echo bot on Heroku

--- app/views.py
+++ app/views.py
@@ -1,11 +1,38 @@
 import time
 
+from flask import request, Response
+from kik.messages import messages_from_json, TextMessage
+
 from app import application
+
+from app import kik
+
 
 @application.route('/')
 def index():
     return "Hej"
 
+
 @application.route('/time')
 def show_time():
     return time.strftime("%c")
+
+
+@application.route('/incoming', methods=['POST'])
+def incoming():
+    if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
+        return Response(status=403)
+
+    messages = messages_from_json(request.json['messages'])
+
+    for message in messages:
+        if isinstance(message, TextMessage):
+            kik.send_messages([
+                TextMessage(
+                    to=message.from_user,
+                    chat_id=message.chat_id,
+                    body=message.body
+                )
+            ])
+
+    return Response(status=200)
