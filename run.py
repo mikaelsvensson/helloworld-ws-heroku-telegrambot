a461fdad2c25e536403d27f1a39918b6c4391ae3
Mikael Svensson <mikael.svensson@klarna.com>
2017-02-05 16:25:10 +0100

    Split code into methods

--- run.py
+++ run.py
@@ -6,32 +6,40 @@ from twx.botapi import InputFileInfo, InputFile
 from app import application
 from app import bot
 
-if os.environ.get('HEROKU') is not None:
+
+def init_heroku_logging():
     import logging
     stream_handler = logging.StreamHandler()
     application.logger.addHandler(stream_handler)
     application.logger.setLevel(logging.DEBUG)
 
-try:
-    bot.update_bot_info().wait()
-    print 'Starting bot %s' % bot.username
 
-    fp = open('app/helloworld-kikbot-herokuapp-com.pem', 'rb')
+def get_certificate():
+    certificate_path = 'app/helloworld-kikbot-herokuapp-com.pem'
+    fp = open(certificate_path, 'rb')
     file_info = InputFileInfo(
-        file_name='app/helloworld-kikbot-herokuapp-com.pem',
+        file_name=certificate_path,
         fp=fp,
         mime_type='text/plain')
+    return InputFile('certificate', file_info)
 
-    certificate = InputFile('certificate', file_info)
 
-    bot.set_webhook(
+def init_bot(telegram_bot):
+    telegram_bot.update_bot_info().wait()
+    print 'Starting bot %s' % telegram_bot.username
+    certificate = get_certificate()
+    telegram_bot.set_webhook(
         url='https://helloworld-telegrambot.herokuapp.com/incoming',
         certificate=certificate,
     ).wait()
+    print 'Webhook configured'
 
-    print 'Certificate: %s' % certificate
 
+if os.environ.get('HEROKU') is not None:
+    init_heroku_logging()
+
+try:
+    init_bot(bot)
+    application.run(debug=True, threaded=True)
 except Exception as e:
     print e
-
-application.run(debug=True, threaded=True)
